81af6e5384aaafa5594c95700940990bd82ecd2f
Rouhis <eeturouhiainen1@gmail.com>
2024-04-02 10:04:14 +0300

    week 2 done

---
+++ SSSF-24-week2-main/src/api/index.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express';
+import categoryRoute from './routes/categoryRoute';
+import speciesRoute from './routes/speciesRoute';
+import animalRoute from './routes/animalRoute';
+import userRoute from './routes/userRoute';
+import authRoute from './routes/authRoute';
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'animals api v1',
+  });
+});
+
+router.use('/auth', authRoute);
+router.use('/categories', categoryRoute);
+router.use('/species', speciesRoute);
+router.use('/animals', animalRoute);
+router.use('/users', userRoute);
+export default router;
