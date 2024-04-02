a2cc52043b1fb5d72b7893bcb6fddb44a19bab26
Rouhis <eeturouhiainen1@gmail.com>
2024-04-02 11:54:54 +0300

    week3 first class done

--- SSSF_ts_graphql-starter-main/src/api/resolvers/animalResolver.ts
+++ SSSF_ts_graphql-starter-main/src/api/resolvers/animalResolver.ts
@@ -1,1 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
 // TODO: animalResolver
+import {Animal} from '../../types/DBTypes';
+const animalData = [
+   {
+      id: '1',
+      animal_name: 'Frank',
+      species: '1',
+   },
+];
+
+export default {
+   Query: {
+      animals: (_parent: undefined, _args: Animal) => {
+       return animalData;
+      },
+   },
+};
--- SSSF_ts_graphql-starter-main/src/api/resolvers/speciesResolver.ts
+++ SSSF_ts_graphql-starter-main/src/api/resolvers/speciesResolver.ts
@@ -1,1 +1,20 @@
+import {Animal} from '../../types/DBTypes';
+
 // TODO: speciesResolver
+const speciesData = [
+  {
+    id: '1',
+    species_name: 'Cat',
+    category: '1',
+  },
+];
+export default {
+  Animal: {
+    species: (parent: Animal) => {
+      console.log(parent);
+      return speciesData.find(
+        (species) => species.id === String(parent.species),
+      );
+    },
+  },
+};
